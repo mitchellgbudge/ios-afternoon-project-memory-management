4584c845eb236f8a29ae924d291ba4f7c1ef0bb3
Mitchell Budge <mitchellbudge@gmail.com>
2019-07-24 13:54:40 -0600

    Initial commit with new Xcode project

---
+++ Contacts MRC/Contacts MRC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Contacts MRC
+//
+//  Created by Mitchell Budge on 7/24/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Contacts MRC/Contacts MRC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Contacts MRC
+//
+//  Created by Mitchell Budge on 7/24/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
