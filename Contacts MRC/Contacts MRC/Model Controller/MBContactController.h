e582a7cb6236969b03f26e84273636393656cfd0
Mitchell Budge <mitchellbudge@gmail.com>
2019-07-24 16:03:36 -0600

    Setup model object

---
+++ Contacts MRC/Contacts MRC/Model Controller/MBContactController.h
@@ -0,0 +1,17 @@
+//
+//  MBContactController.h
+//  Contacts MRC
+//
+//  Created by Mitchell Budge on 7/24/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MBContactController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Contacts MRC/Contacts MRC/Model Controller/MBContactController.m
@@ -0,0 +1,13 @@
+//
+//  MBContactController.m
+//  Contacts MRC
+//
+//  Created by Mitchell Budge on 7/24/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+#import "MBContactController.h"
+
+@implementation MBContactController
+
+@end
